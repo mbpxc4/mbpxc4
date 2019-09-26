be4cf3ef4e89462ec5206fbb5c8be38ff133f6e0
mbpxc4 <54566364+mbpxc4@users.noreply.github.com>
2019-09-25 23:56:50 -0500

    Update change_grade_test.py

--- assignment-five/change_grade_test.py
+++ assignment-five/change_grade_test.py
@@ -1,5 +1,12 @@
 import pytest
-
+"""
+    This program is used to change a grade in the grading book.
+    (e.x. If the teacher needed to apply a curve to a grade)
+    The first test will pass because the updated_grade isn't
+    the same as the previous_grade.  The second test will not
+    pass because the updated_grade is the same as the 
+    previous_grade.
+"""
 def change_grade(old_grade):
     new_grade = old_grade + 4
     return new_grade
